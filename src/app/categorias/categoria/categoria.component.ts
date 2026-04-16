e33dd257857efffd036811d80c6ccfacba9e52d9
Allan Moreira <allanfelipe.moreira@gmail.com>
2026-04-15 22:50:41 -0300

    Formulário reativo

--- src/app/categorias/categoria/categoria.component.ts
+++ src/app/categorias/categoria/categoria.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-categoria',
@@ -6,4 +7,19 @@ import { Component } from '@angular/core';
   templateUrl: './categoria.component.html',
   styleUrl: './categoria.component.scss',
 })
-export class CategoriaComponent {}
+export class CategoriaComponent {
+  camposForm: FormGroup;
+
+  constructor() {
+    this.camposForm = new FormGroup({
+      nome: new FormControl('', Validators.required),
+      descricao: new FormControl('', Validators.required),
+    })
+  }
+
+  salvar(){
+    console.log('Valores digitados', this.camposForm.value);
+    console.log('Válido?', this.camposForm.valid);
+  }
+
+}
